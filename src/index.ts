d97bb6d1ecb3f25cae80333f4bb1bbefba7d1403
guilhermeSmartins <mineg4500@gmail.com>
2022-02-13 17:36:51 -0300

    learning how to use loader class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Graphics } from 'pixi.js';
+import { Application, Graphics, Loader } from 'pixi.js';
 
 const app = new Application({
   width: innerWidth,
@@ -7,16 +7,26 @@ const app = new Application({
 
 document.body.appendChild(app.view);
 
-const graphics = new Graphics();
-graphics.beginFill(0xffffff);
-graphics.drawRect(20, 50, 100, 80);
-graphics.endFill();
+const loader = Loader.shared;
 
-const graphic2 = new Graphics();
-graphic2.beginFill(0xff00000);
-graphic2.drawCircle(50, 100, 30);
-graphic2.endFill();
+loader
+  .add('img1', 'https://cdn.pixabay.com/photo/2014/05/26/13/32/butterfly-354528__480.jpg')
+  .add('img2', 'https://cdn.pixabay.com/photo/2016/09/04/12/20/pink-1643831_960_720.jpg')
+  .load((l, r) => {
+    const img1 = l.resources.img1;
+    const img2 = l.resources.img2;
 
+    console.log(img1, img2)
+  });
 
-app.stage.addChild(graphics);
-app.stage.addChild(graphic2);
+loader.onProgress.add((l, r) => {
+  console.log(loader.progress);
+});
+
+loader.onLoad.add((e) => {
+  console.log(e);
+});
+
+loader.onComplete.add((e) => {
+  console.log('loading the assets completed');
+});
